bdf43ad0c6714c96bd9cbca8dc96c5c50c985c1b
tamirgen <tamirgenkin@gmail.com>
2021-11-05 14:14:49 +0000

    change the game grid from 3 to 4 rows and columns

--- run.py
+++ run.py
@@ -7,9 +7,8 @@ class Board:
         self.board = ['.'] * 16
     
     def to_string(self):
-        return '{}|{}|{}\n-----\n{}|{}|{}\n-----\n{}|{}|{}\n'.format(*self.board)
-class Game:
-    pass        
+        return '{}|{}|{}|{}\n-------\n{}|{}|{}|{}\n-------\n{}|{}|{}|{}\n'.format(*self.board)
+       
 
 print(Board().to_string())
 
