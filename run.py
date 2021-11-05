19dd0ef68db8f1eb3361276f3db79bd30f0f6230
tamirgen <tamirgenkin@gmail.com>
2021-11-05 14:11:36 +0000

    create the Board class

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+class Board:
+    """
+    Creates the game board
+    """
+    def __init__(self):
+        self.board = ['.'] * 16
+    
+    def to_string(self):
+        return '{}|{}|{}\n-----\n{}|{}|{}\n-----\n{}|{}|{}\n'.format(*self.board)
+class Game:
+    pass        
+
+print(Board().to_string())
+
+
