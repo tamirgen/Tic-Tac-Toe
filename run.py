93b2b3ffd073b56750c98210057997536303dd9f
tamirgen <tamirgenkin@gmail.com>
2021-11-09 15:02:12 +0000

    change the board to 3X3 and add a winnig statement

--- run.py
+++ run.py
@@ -1,13 +1,44 @@
 
+class Game:
+    """
+    Run the game and swapping turns between 2 palyers
+    """
+    def __init__(self, player1, player2):
+        self.board = Board()
+        self.player = [player1, player2]
+        self.turn = False
+
+    def play(self):
+        while True:
+            current_player = self.player[int(self.turn)]
+
+            print(self.board.to_string())
+            move = int(input(f'this is the current board, {current_player.name} make a move 0-8.\n'))
+            while True:
+                """
+                This Loop is checking if there is a winner and if the palyers made a legal move
+                """
+                try:
+                    is_winner = self.board.make_move(current_player, move)
+                    break
+                except ValueError:
+                    move = int(input('Ilegal move, pleae try again.'))
+                if is_winner:
+                    break    
+
+            self.turn = not self.turn
+            print(self.board.to_string())
+            print(f'Congratulations {current_player.name}, you win!')
+
 class Board:
     """
     Creates the game board
     """
     def __init__(self):
-        self.board = ['.'] * 16
-    
+        self.board = ['.'] * 9
+   
     def to_string(self):
-        return '{}|{}|{}|{}\n-------\n{}|{}|{}|{}\n-------\n{}|{}|{}|{}\n'.format(*self.board)
+        return '{}|{}|{}\n-----\n{}|{}|{}\n-----\n{}|{}|{}\n'.format(*self.board)
 
     def make_move(self, player, place):
         """
@@ -23,18 +54,16 @@ class Board:
         Check winning combinations
         """
         winning_positions = [
-            [1,2,3,4],
-            [5,6,7,8],
-            [9,10,11,12],
-            [13,14,15,16],
-            [1,5,9,13],
-            [2,6,10,14],
-            [3,7,11,15],
-            [4,8,12,16],
-            [1,6,11,16],
-            [4,7,10,13],
-           
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
             ]
+
         return any([all([self.board[x] == marker for x in pos]) for pos in winning_positions])
 
        
@@ -46,38 +75,8 @@ class Player:
         self.name = name
         self.marker = marker
        
-class Game:
-    """
-    Run the game and swapping turns between 2 palyers
-    """
-    def __init__(self, player1, player2):
-        self.board = Board()
-        self.player = [player1, player2]
-        self.turn = False
-
-    def play(self):
-        while True:
-            current_player = self.player[int(self.turn)]
-
-            print(self.board.to_string())
-            move = int(input(f'this is the current board, {current_player.name} make a move 0-15.\n'))
-            while True:
-                """
-                This Loop is checking if there is a winner and if the palyers made a legal move
-                """
-                try:
-                    is_winner = self.make_move(current_player, move)
-                    break
-                except ValueError:
-                    move = int(input('Ilegal move, pleae try again.'))
-                if is_winner:
-                    break    
-
-
-            self.turn = not self.turn
-
-
-Game(Player('Tom', 'x'), Player('Regina', '0')).play()
+       
+Game(Player('Tom', 'x'), Player('Regina', 'o')).play()
 
 
 
